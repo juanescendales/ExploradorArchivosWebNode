5e14c144c64674f96fc415e789bb0623e3dab4c1
Juan Esteban Cendales <juanescendales@gmail.com>
2020-02-18 18:04:59 -0500

    Revert "Algunos Cambios, falta corregir funcionalidades"
    This reverts commit 90b729233c10db6da072e880d9c0233d4fe50b85.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
