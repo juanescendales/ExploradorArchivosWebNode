e3c6a29d37df4a7b1450e2ab66dded846af14385
juanescendales <jcendaless@unal.edu.co>
2020-02-19 12:35:48 -0500

    Integracion de rutas estaticas

--- src/app.js
+++ src/app.js
@@ -10,15 +10,15 @@ const indexRoutes =require('./routes/index.js')
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(express.static('public'));
+
 // middlewares
 app.use(morgan('dev'));
-
+app.use(express.urlencoded({extended:false}));
 // routes
 app.use('/',indexRoutes);
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(__dirname + '/public'));
 
 // listening the Server
 app.listen(app.get('port'), () => {
